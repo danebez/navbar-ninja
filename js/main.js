b873cfb1e6479d44a3041f79eb85c471e4889288
Dane Bezuidenhout <danebez@gmail.com>
2016-03-08 14:10:20 +0200

    Attempting to use query parameter to identify site to navbar.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
+// preload images
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        (new Image()).src = this;
+    });
+}
+
+preload([
+    'img/pet_heaven_small.png',
+    'img/pet_heaven_small_greyscale.png',
+    'img/faithful_to_nature_small.png',
+    'img/faithful_to_nature_small_greyscale.png',
+    'img/cyber_cellar_small.png',
+    'img/cyber_cellar_small_greyscale.png',
+]);
+
+// see: http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 $(document).ready(function() {
-
-    // preload images
-
-    function preload(arrayOfImages) {
-        $(arrayOfImages).each(function(){
-            (new Image()).src = this;
-        });
-    }
-
-    preload([
-        'img/pet_heaven_small.png',
-        'img/pet_heaven_small_greyscale.png',
-        'img/faithful_to_nature_small.png',
-        'img/faithful_to_nature_small_greyscale.png',
-        'img/cyber_cellar_small.png',
-        'img/cyber_cellar_small_greyscale.png',
-    ]);
 
     // get the current site and activate the relevant image
 
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
-
     site = getParameterByName('site');
-    alert(site);
 
     if (site == 'pet_heaven') {
         $('img#pet_heaven').attr('src', 'img/pet_heaven_small.png');
@@ -39,7 +39,7 @@ $(document).ready(function() {
     } else if (site == 'cyber_cellar') {
         $('img#cyber_cellar').attr('src', 'img/cyber_cellar_small.png');
     } else {
-        alert('Unknown site!');
+        alert('Unknown site: ' + site);
     }
 
     // mouse over handlers
